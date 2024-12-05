40d453a19f8b85af191443d40a45f3b79c59be23
DiegoGomes-1 <diegogomes.dev@gmail.com>
2024-12-05 20:41:00 -0300

    Jeito mais fácil de alterar de alterar as labels da pagina de edição. Modificações no arquivo forms.py

--- taskmasterapp/forms.py
+++ taskmasterapp/forms.py
@@ -1,13 +1,21 @@
-from django import froms
+from django import forms
+from django.forms import ModelForm, TextInput
 from .models import Task 
 
-class TaskForm(form.ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'due_date', 'priority']
         widgets = {
 
+                'title': TextInput(attrs={'style': 'color: blue'}),
                 'due_date': forms.DateInput(attrs={'type': 'date'}),
                 'priority': forms.Select(),
 
+        }
+
+        labels = {
+            "title": "Título",
+            "due_date": "Data",
+            "priority": "Prioridade"
         }
